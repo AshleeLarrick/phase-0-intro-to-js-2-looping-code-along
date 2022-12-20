b7a112531a86423dc6df43b6d1e687a4299c0463
ashlee.larrick <ashleemareelarrick323@gmail.com>
2022-12-20 11:48:44 -0700

    commit tree c65e3ab2c9c24b0fd7f0bd8d6c271a33e7d904ea

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+const names = ["Charlie", "Samip", "Ali"];
+const event = "birthday";
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(names, event) {
+    const thanks = []
+    for (let i = 0; i < names.length; i++) {
+        thanks.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return thanks;
+}
+function countDown(integer) {
+    while (integer >= 0) {
+        console.log(integer--);
+    }
+}
